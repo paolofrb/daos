403c762dd8491fe077d3ce17e96d8b9aeb58087f
Fan Yong <fan.yong@intel.com>
2019-03-11 01:10:24 +0800

    DAOS-1441 dtx: resync non-committed DTX status
    For performance consideration, the leader caches the committable
    DTXs (in DRAM) and commits them asychronously. If leader failed,
    there may be some committable DTXs that are not committed. Because
    DTX status affects the visibility of related data record that is
    modified via the DTX, we need to resync former cached DTXs state,
    otherwise, accessing related data records will get wrong results,
    including the rebuilding objects process.
    
    This patch introduces the logic to resync DTX status when the
    container is opened or before scanning the objects for rebuild.
    Each replica scans its local active-DTX table, for the leader,
    it check with other replicas about the DTX state. As long as one
    replica reports 'committed' or all the replicas as 'prepared',
    then such DTX can be committed, otherwise, the DTX will be aborted.
    
    The patch also enhance the rebuilding object logic as following:
    
    1. For each server, it will not start the object scan until the
       local DTX resync is done.
    
    2. If the server is the leader for some object's shard, then it
       can send such shard to the in-rebuilding target.
    
    3. The in-rebuilding target only can fetch data from leaders to
       avoid missing any data record.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: I3a1916590d8e13027fb9c48a7575650e23a06f30

--- src/rebuild/scan.c
+++ src/rebuild/scan.c
@@ -548,14 +548,13 @@ out:
 
 #define LOCAL_ARRAY_SIZE	128
 static int
-placement_check(uuid_t co_uuid, daos_unit_oid_t oid,
-		daos_epoch_t epoch, void *data)
+placement_check(uuid_t co_uuid, vos_iter_entry_t *ent, void *data)
 {
 	struct rebuild_scan_arg	*arg = data;
 	struct rebuild_tgt_pool_tracker *rpt = arg->rpt;
-	struct pl_obj_layout	*layout = NULL;
 	struct pl_map		*map = NULL;
 	struct daos_obj_md	md;
+	daos_unit_oid_t		oid = ent->ie_oid;
 	unsigned int		tgt_array[LOCAL_ARRAY_SIZE];
 	unsigned int		shard_array[LOCAL_ARRAY_SIZE];
 	unsigned int		*tgts = NULL;
@@ -608,9 +607,26 @@ placement_check(uuid_t co_uuid, daos_unit_oid_t oid,
 		 * myrank should always not equal to tgt_rebuild. XXX
 		 */
 		if (myrank != tgts[i]) {
+			daos_unit_oid_t		tmp = oid;
+
+			tmp.id_shard = shards[i];
+			rc = ds_pool_check_leader(rpt->rt_pool_uuid, &tmp,
+						  rpt->rt_rebuild_ver, NULL);
+			if (rc == 0) {
+				/* The leader shard is not on current server,
+				 * then current server does not know whether
+				 * related DTX(s) for current shard have been
+				 * committed or not. Skip the shard that will
+				 * be handled by the leader on another server.
+				 */
+				D_DEBUG(DB_REBUILD, "Skip non-leader replica "
+					DF_UOID".\n", DP_UOID(tmp));
+				continue;
+			}
+
 			rc = rebuild_object_insert(arg, tgts[i], shards[i],
 						   rpt->rt_pool_uuid, co_uuid,
-						   oid, epoch);
+						   oid, ent->ie_epoch);
 			if (rc)
 				D_GOTO(out, rc);
 		} else {
@@ -619,9 +635,6 @@ placement_check(uuid_t co_uuid, daos_unit_oid_t oid,
 		}
 	}
 out:
-	if (layout != NULL)
-		pl_obj_layout_free(layout);
-
 	if (tgts != tgt_array && tgts != NULL)
 		D_FREE(tgts);
 
@@ -652,8 +665,8 @@ rebuild_scanner(void *data)
 	while (daos_fail_check(DAOS_REBUILD_TGT_SCAN_HANG))
 		ABT_thread_yield();
 
-	return ds_pool_obj_iter(rpt->rt_pool_uuid, arg->callback,
-				arg->arg);
+	return ds_pool_iter(rpt->rt_pool_uuid, arg->callback, arg->arg,
+			    rpt->rt_rebuild_ver, VOS_ITER_OBJ);
 }
 
 static int
